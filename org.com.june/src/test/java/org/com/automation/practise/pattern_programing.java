cd3286d43f4d0d5cea2493b61d3ccc58903e7c98
sagarjadhav180 <sagar.j@moentek.com>
2019-07-30 21:43:19 +0530

    pattern programing class added

---
+++ org.com.june/src/test/java/org/com/automation/practise/pattern_programing.java
@@ -0,0 +1,121 @@
+package org.com.automation.practise;
+
+import org.testng.annotations.Test;
+
+public class pattern_programing {
+
+	
+	@Test(enabled=false)
+	public void test1(){
+		int row=3;
+		int star=1;
+		int hash=5;
+		
+		for(int i=1;i<=row;i++){
+			
+			for(int j=1;j<=hash;j++){
+				
+				if(i==1 && j==1 || i==2 && j>3 || i==3 && j<4){
+					System.out.print("*");
+				}
+				else{
+		System.out.print("#");
+				}
+			}
+			
+			
+			
+			System.out.println();
+		}
+
+
+			
+	}
+	
+	@Test(enabled=false)
+	public void test2(){
+		int row=5;
+		int star=5; 
+		int space=0;
+		
+		for(int i=0;i<=row;i++){
+			
+			for(int k=0;k<=space;space++){
+				System.out.print(" "); 
+			}
+			
+			for(int j=1;j<=star;j++){
+				System.out.print("*");
+			}  
+			
+			System.out.println(); 
+			star--;
+			space++;
+		}
+		 
+	}	
+	
+	@Test(enabled=false)
+	public void test3()
+	{
+		int star=5;
+		int space=0; 
+		int row=5;
+		int mid=(row/2);
+	int test;	
+		
+		for(int i=1;i<=row;i++){
+			
+			for(int j=0;j<=space;j++){
+				System.out.print(" ");
+			}
+			
+			for(int k=1;k<=star;k++){
+				System.out.print("*");
+			}
+			System.out.println();
+			if(i<=mid){
+			star=star-2;
+			space++;
+			}
+			else{
+				star=star+2;
+				space--;
+			
+			}
+			
+		}
+			
+		
+	}
+	
+	
+	@Test(enabled=true)
+	public void test4(){
+	
+		int star=1;
+		int row=5;
+		
+		for(int i=1;i<=row;i++){
+			for(int j=1;j<=star;j++){
+				
+				System.out.print("*");
+			}
+			
+			star=star+2;
+			System.out.println();
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
